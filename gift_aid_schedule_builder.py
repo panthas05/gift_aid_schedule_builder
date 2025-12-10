a35550b3a1da06397eaa951d88ee2c8a482eafb7
panthas <>
2025-12-10 06:46:12 +0000

    Add support for excel spreadsheet

--- gift_aid_schedule_builder.py
+++ gift_aid_schedule_builder.py
@@ -4,11 +4,11 @@
 import build_output_directory
 from logic.parsing import parse_transactions_csv, parse_declarations_csv
 import models
+import arguments
+import sys
 
 
-def main() -> None:
-    # TODO: extend to take a flag, which specifies either "excel" or "libre", and uses
-    # the corresponding spreadsheet when generating the output.
+def main(spreadsheet_type: arguments.SpreadsheetType) -> None:
     transaction_rows = parse_transactions_csv.parse_transactions_file()
     declarations = [
         models.DonorDeclaration.from_declaration_row(dr)
@@ -17,8 +17,22 @@ def main() -> None:
     build_output_directory.build_output_directory(
         transaction_rows,
         declarations,
+        spreadsheet_type,
     )
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        spreadsheet_type = arguments.parse_arguments(sys.argv)
+        main(spreadsheet_type)
+    except arguments.NoSpreadsheetTypeProvided:
+        print(
+            "Please specify the type of spreadsheet you wish to be output "
+            "(hint: use --output=excel if you use excel or --output=libre if "
+            "you use libreoffice)."
+        )
+    except arguments.UnexpectedSpreadsheetType as e:
+        print(
+            f'Unexpected spreadsheet type "{e.passed_value}", expected either '
+            '"excel" or "libre".'
+        )
